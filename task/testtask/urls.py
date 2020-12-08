5d7fa9cc2feee09b825dd33e1796ebd533879303
Anton_deltaTime <space.volk@gmail.com>
2020-12-09 00:19:11 +0500

    rename html page

--- task/testtask/urls.py
+++ task/testtask/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 
 urlpatterns = [
-    path('html', views.IndexHTMLView.as_view(), name='task_html'),
-    path('json', views.IndexJsonView.as_view(), name='task_json')
+    path('index.html', views.IndexHTMLView.as_view(), name='task_html'),
+    path('index.json', views.IndexJsonView.as_view(), name='task_json')
 ]
